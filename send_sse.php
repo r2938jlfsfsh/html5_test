c53410aa8ee507394b7cbf05d14fc425743b14b1
anon <russell_g1974@yahoo.com.au>
2017-07-17 07:20:35 +1000

    check

--- send_sse.php
+++ send_sse.php
@@ -2,7 +2,9 @@
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-$new_data = rand(0,1000);
+//generate random number for demonstration
+$new_data = rand(0, 1000);
+//echo the new number
 echo "data: New random number: $new_data";
 flush();
 ?>
