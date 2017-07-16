96fd9789473027226a74708d17ead48a1ef36c87
anon <russell_g1974@yahoo.com.au>
2017-07-17 07:28:33 +1000

    check

--- send_sse.php
+++ send_sse.php
@@ -5,7 +5,6 @@ header('Cache-Control: no-cache');
 //generate random number for demonstration
 $new_data = rand(0, 1000);
 //echo the new number
-//echo "retry: 5000\ndata: {$newdata}\n\n";
-echo "data: {$new_data}\n\n";
+echo "retry: 2000\ndata: {$new_data}\n\n";
 flush();
 ?>
